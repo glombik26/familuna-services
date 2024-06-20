d7d5fee077d392e72057448183ee1247d0adf149
Michael Glombik <michael.glombik@netempire.software>
2024-06-20 17:48:36 +0200

    initial commit

---
+++ src/main/java/com/glombik26/familuna/FamilunaApplication.java
@@ -0,0 +1,13 @@
+package com.glombik26.familuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilunaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glombik26/familuna/FamilunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glombik26.familuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
